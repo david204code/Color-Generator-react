bfc1904010398b42b0660f83f8baee01f05c71bf
David Wu <david204code@gmail.com>
2021-01-13 16:04:19 +0000

    Set the state value, add handleSubmit function and returning the basic structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React, { useState } from 'react'
 import SingleColor from './SingleColor'
-
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  // state values
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Thank you Lord Jesus');
+  }
+
+  return (
+    <>
+      <section className ="constainer">
+        <h3>Color Generator</h3>
+        <form onSubmit ={handleSubmit}>
+          <input 
+            type ="text" 
+            value ={color} 
+            onChange ={(e) => setColor(e.target.value)}
+            placeholder ="#f15025"
+          />
+          <button className ="btn" type ="submit">Submit</button>
+        </form>
+      </section>
+
+      <section className ="colors">
+        <h4>Literating list</h4>
+      </section>
+    </>
+  );
 }
 
 export default App
