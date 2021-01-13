d0075f4babf6c334f75d07e3bca016b060aa380b
David Wu <david204code@gmail.com>
2021-01-13 16:57:41 +0000

    Add the className with condition for red borader on the Input element

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ function App() {
             value ={color} 
             onChange ={(e) => setColor(e.target.value)}
             placeholder ="#f15025"
+            className ={`${error ? 'error' : null}`}
           />
           <button className ="btn" type ="submit">Submit</button>
         </form>
